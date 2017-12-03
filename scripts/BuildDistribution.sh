8d2b37f73edacf87340bd722c0a785eefd734493
xingze.he <xingze.he@zenjoy.net>
2017-12-03 19:39:04 +0800

    固定版本号为3.0-alpha5

--- scripts/BuildDistribution.sh
+++ scripts/BuildDistribution.sh
@@ -8,9 +8,9 @@ rm -Rf build/
 rm -Rf CocosBuilder/build/
 
 # Update version for about box
-echo "Version: $1" > version/Version.txt
-echo -n "GitHub: " >> version/Version.txt
-git rev-parse --short=10 HEAD >> version/Version.txt
+# echo "Version: $1" > version/Version.txt
+# echo -n "GitHub: " >> version/Version.txt
+# git rev-parse --short=10 HEAD >> version/Version.txt
 touch version/Version.txt
 
 # Clean and build CocosBuilder
